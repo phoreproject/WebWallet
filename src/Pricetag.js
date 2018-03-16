9b9a20778e29d67779df93a23029c487f477f1dd
Diógenes Augusto Fernandes Hermínio <diofeher@gmail.com>
2018-03-15 22:44:38 -0300

    Price tag

--- src/Pricetag.js
+++ src/Pricetag.js
@@ -1,27 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import { fetchPrices } from "./actions/index";
 
-export default class Pricetag extends Component {
-  render() {
-    return <div className="body" id="pricetag">
-      <div className="body-heading">
-        Pricetag
-          </div>
-      <div className="body-content prices">
-        <div className="row">
-          <div className="column">USD<span className="price-value" id="USD">$4,70</span></div>
-          <div className="column">EUR<span className="price-value" id="EUR">€3,90</span></div>
-          <div className="column">AUD<span className="price-value" id="AUD">A$5,98</span></div>
-          <div className="column">CAD<span className="price-value" id="CAD">C$5,83</span></div>
-        </div>
-        <div className="row">
-          <div className="column">GBP<span className="price-value" id="GBP">£3,46</span></div>
-          <div className="column">HKD<span className="price-value" id="HKD">H$36,74</span></div>
-          <div className="column">CNY<span className="price-value" id="CNY">¥30,49</span></div>
-          <div className="column">NZD<span className="price-value" id="NZD">N$6,55</span></div>
-        </div>
-        {/* 190px for 3 rows */}
-      </div>
-    </div>
+class Pricetag extends Component {
+  constructor(props) {
+    super(props)
   }
-
+  async componentDidMount() {
+    await this.props.fetchPrices()
+    // console.log('prices', this.props.prices);
+  }
+  render() {
+    return (
+      <div className="body" id="pricetag">
+        <div className="body-heading">
+          Pricetag
+          </div>
+        <div className="body-content prices">
+          <div className="row">
+            <div className="column">USD<span className="price-value" id="USD">${this.props.prices.USD}</span></div>
+            <div className="column">EUR<span className="price-value" id="EUR">€{this.props.prices.EUR}</span></div>
+            <div className="column">AUD<span className="price-value" id="AUD">A${this.props.prices.AUD}</span></div>
+            <div className="column">CAD<span className="price-value" id="CAD">C${this.props.prices.CAD}</span></div>
+          </div>
+          <div className="row">
+            <div className="column">GBP<span className="price-value" id="GBP">£{this.props.prices.GBP}</span></div>
+            <div className="column">HKD<span className="price-value" id="HKD">H${this.props.prices.HKD}</span></div>
+            <div className="column">CNY<span className="price-value" id="CNY">¥{this.props.prices.CNY}</span></div>
+            <div className="column">NZD<span className="price-value" id="NZD">N${this.props.prices.NZD}</span></div>
+          </div>
+          {/* 190px for 3 rows */}
+        </div>
+      </div>
+    )
+  }
 }
+
+function mapStateToProps({ prices }) {
+  return { prices }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPrices }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Pricetag);
--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';  
+import { store } from './redux';
+
+ReactDOM.render(  
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
